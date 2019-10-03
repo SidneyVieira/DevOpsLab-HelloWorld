2aca63e81b53b45e0369a396e97700535b0abf05
files.sidney <files.sidney@gmail.com>
2019-10-03 13:10:51 -0400

    UPDATE travis.yml

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Curso de DevOps com Sidney :)")
+        self.assertRegex(result.data.decode(), "Curso de DevOps com Sidney")
 
 
 if __name__ == "__main__":
